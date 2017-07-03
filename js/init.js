895a36e38a30dc2da82c0a242b3dfb3cf0dfbab8
RondinelliCarlos <rondinellicarlos@gmail.com>
2017-07-03 20:09:02 -0300

    first update

---
+++ js/init.js
@@ -0,0 +1,11 @@
+    $('.button-collapse').sideNav({
+    	menuWidth:250,
+    	closeOnClick: true,
+    	draggable: true,
+    });
+
+        $('.scrollspy').scrollSpy();
+
+        $(document).ready(function(){
+      $('.slider').slider();
+    });
